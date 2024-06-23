8d34a7738c12fc903f1ff051fc26d93624c6f123
Sally Vu <88215189+HauVu-flinders@users.noreply.github.com>
2024-06-23 22:41:05 +0930

    updated OnGet method of ListQuestions model

--- QuizApp/Pages/Quizzes/ListQuizzes.cshtml.cs
+++ QuizApp/Pages/Quizzes/ListQuizzes.cshtml.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using QuizApp.Data;
 using QuizApp.Models;
 
 namespace QuizApp.Pages.Quizzes
